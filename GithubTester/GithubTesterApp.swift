04a786cf079479b012f7220540afa12cde24bf6e
Josh Knowles <59399+chasing@users.noreply.github.com>
2023-09-11 13:53:29 -0500

    Initial Commit

---
+++ GithubTester/GithubTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTesterApp.swift
+//  GithubTester
+//
+//  Created by Josh Knowles on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
